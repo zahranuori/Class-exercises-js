cee781e65a02ee45d295f69b4bc2f51df1cd1d91
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2021-10-16 00:52:23 +0330

    word search

---
+++ repeatFigures.js
@@ -0,0 +1,16 @@
+function copyNumber(number) {
+    let result = 0
+    for (let i = 0; i < number; i++) {
+        result += ((10 ** i) * number)
+    }
+    return result
+}
+
+function repeatNumber(number) {
+    let numb = number.toString().split('')
+    for (let item of numb) {
+        console.log('%s', item, copyNumber(item))
+    }
+}
+
+console.log(repeatNumber(197))
---
+++ searchWord.js
@@ -0,0 +1,37 @@
+// function searchWord(string1 , string2) {
+//     let count=0;
+//     for(let i=0;i< string1.length;i++){
+//         for(let z=0;z< string2.length;z++){
+//             if (string2[z] != string1[i+z]){
+//                 break;
+//             }
+//             if (z === string2.length -1){
+//                 count++;
+//
+//             }
+//         }
+//
+//     }
+//     return count
+// }
+//
+// console.log(searchWord('zahra' , 'zah'))
+
+
+// ----------------------   hello world   --------------------------------
+
+const wordSearch = (str1 , str2) => {
+    if (typeof str1 !== 'string' || typeof str2 !== 'string')
+        return -1
+    if(str2.length > str1.length)
+        return -1
+    for (let i = 0 ; i <= str1.length - str2.length ; i++) {
+        let temp = str1.substr(i , str2.length);
+        if (temp === str2) {
+            return i;
+        }
+    }
+    return -1
+}
+
+console.log(wordSearch('reeezaaa' , 'eza'))
