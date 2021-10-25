8792c5083e320410d8a399e8b7367ea0ed507cfd
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2021-10-25 12:36:16 +0330

    offset index add new code commit

--- offsetIndex.js
+++ offsetIndex.js
@@ -1,4 +1,16 @@
-let offset = [1, 2, 3, 4, 5,], index = 4;
-for (let i = index; i < offset.length; i++)
-    offset.unshift(offset.pop());
-console.log(offset);
+// let offset = [1, 2, 3, 4, 5,], index = 4;
+// for (let i = index; i < offset.length; i++)
+//     offset.unshift(offset.pop());
+// console.log(offset);
+
+function moveArrayItemToNewIndex(arr, offset, index) {
+    if (index >= arr.length) {
+        let k = index - arr.length + 1;
+        while (k--) {
+            arr.push(undefined);
+        }
+    }
+    arr.splice(index, 0, arr.splice(offset, 1)[0]);
+    return arr;
+};
+console.log(moveArrayItemToNewIndex(["a","b","c","d"], 2, 1));
